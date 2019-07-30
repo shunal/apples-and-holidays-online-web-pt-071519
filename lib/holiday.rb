004039235619a787713045458ef8eaae43b4c7c2
Steven Hunal <stevehunal@gmail.com>
2019-07-30 19:19:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,6 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 holiday_hash.each do |season, holiday|
   holiday[1]
 end
+end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
