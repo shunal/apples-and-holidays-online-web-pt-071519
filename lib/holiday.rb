a7bdfce4884c4324645db9ab6292f08565f46090
Steven Hunal <stevehunal@gmail.com>
2019-07-30 19:21:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,8 +20,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   
 holiday_hash.each do |season, holiday|
+  if holiday.count >= 2
+    holiday[2]
 end
-holiday[1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
